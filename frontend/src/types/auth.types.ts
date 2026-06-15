d4c69b239ddf5e550bb6018e14a3ead0b2e61c97
Nihar <niharpatil.codes@gmail.com>
2026-06-15 16:44:48 +0530

    Implemented the auth type

--- frontend/src/types/auth.types.ts
+++ frontend/src/types/auth.types.ts
@@ -1,3 +1,16 @@
-export interface Auth {
-  id: string;
+import { User } from "lucide-react";
+
+export interface User {
+  id : string;
+  name : string;
+  email : string;
+  avatar?: string;
 }
+export interface AuthState {
+  user : User | null;
+  isAuthenticated : boolean;
+
+  login: (user: User) => void ;
+  logout: () => void;
+}
+
